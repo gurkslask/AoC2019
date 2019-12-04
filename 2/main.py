6575a84748fbec4b8d464b5c915d90b7425c1030
Alex <gurkslask@gmail.com>
2019-12-04 15:50:35 +0100

    end of day save

--- 2/main.py
+++ 2/main.py
@@ -25,25 +25,34 @@ def calc(stringen, prod):
     return l[0]
 
 def calcAdv(stringen ):
-    l = stringen.split(",")
-    for c, i in enumerate(l):
-        l[c] = int(l[c])
+    ll = stringen.split(",")
+    for c, i in enumerate(ll):
+        ll[c] = int(ll[c])
     # print("splittad: ", l)
-    done = False
-    i = 0
-    while not done:
-        ll = l
-        # print("Start Loop: ", i)
-        if l[i] == 1:
-            # print("{} = {} + {}".format(l[l[c+3]],l[l[c+1]], l[l[c+2]] ))
-            l[l[i+3]] = l[l[i+1]] +l[l[i+2]] 
-        elif l[i] == 99:
-            done = True
-        elif l[i] == 2:
-            # print("{} = {} + {}".format(l[l[c+3]],l[l[c+1]], l[l[c+2]] ))
-            l[l[i+3]] = l[l[i+1]] *l[l[i+2]] 
-        # print("Efter loop: ", l)
-        i += 4
+    for noun in range(0,100):
+        for verb in range(0,100):
+            i = 0
+            l = ll
+            done = False
+            l[1] = noun
+            l[2] = verb
+            while not done:
+                # print("Start Loop: ", i)
+                if l[i] == 1:
+                    # print("{} = {} + {}".format(l[l[c+3]],l[l[c+1]], l[l[c+2]] ))
+                    l[l[i+3]] = l[l[i+1]] +l[l[i+2]] 
+                elif l[i] == 99:
+                    done = True
+                elif l[i] == 2:
+                    # print("{} = {} + {}".format(l[l[c+3]],l[l[c+1]], l[l[c+2]] ))
+                    print(l)
+                    print(i)
+                    print(l[i+3] , l[i+1] ,l[i+2] )
+                    l[l[i+3]] = l[l[i+1]] *l[l[i+2]] 
+                # print("Efter loop: ", l)
+                i += 4
+            print(l[0])
+
     print(l)
     return l[0]
 
@@ -51,5 +60,6 @@ if __name__ == "__main__":
     with open("input.txt", 'r') as f:
         d = f.readlines()
         print(calc(d[0], True))
+        print(calcAdv(d[0]))
 
 
---
+++ 3/main.py
@@ -0,0 +1,16 @@
+def calc(string1, string2):
+    mapp = [[{}]] 
+    d = {
+            "R": moveright()
+        }
+
+
+    def moveright(m, org_pos, move):
+        return m
+    
+if __name__ == "__main__":
+    with open("input.txt", 'r') as f:
+        d = f.readlines()
+        print(calc(d[0], True))
+
+
